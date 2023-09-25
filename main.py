4b228c8e834d6f67b22fdbc6c654f7e17876053e
Victoria Cabrera-Moglia <victoriacabrera-moglia@hey.com>
2023-09-25 19:17:33 -0400

    image generation is sort of working

--- main.py
+++ main.py
@@ -17,16 +17,22 @@ if __name__ == "__main__":
             {"role": "system", "content": "You are a professional children's book author creating material for a publishing house. You are going to receive a story concept. Turn it into a complete narrative arc, and produce a precis for that narrative arc which is somewhat like a story board. The story should be a complete story with up to 3 characters."},
             {"role": "user", "content": STORY_PROMPT}
         ],
-        temperature=2
+        temperature=.8
     )["choices"][0]["message"]["content"]
     print(story_outline)
     story_beats = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
-            {"role": "system", "content": "You are a professional children's book author creating material for a publishing house. You are going to receive a story concept. Turn it into twelve scenes that could be illustrated in a children's book. Think about it like a storyboard artist. "},
+            {"role": "system", "content": "You are a professional children's book author creating material for a publishing house. You are going to receive a story concept. Turn it into twelve scenes that could be illustrated in a children's book. Think about it like a storyboard artist. Write a paragraph for each. Don't skimp! Separate scenes only with word scene. Start the response with the word scene."},
             {"role": "user", "content": story_outline}
         ],
-        temperature=2, 
-        tokens=2000
+        temperature=.8, 
+        max_tokens=4000
     )["choices"][0]["message"]["content"]
-    print(story_beats)
+    story_beats = story_beats.split("Scene")[1:]
+    urls = []
+    for beat in story_beats[0:2]:
+        image_resp = openai.Image.create(prompt="Soft, cartoon, illustrated. Children's book style." + beat, n=4, size="1024x1024")
+        url = image_resp['data'][0]['url']
+        urls.append(url)
+    ipdb.set_trace()
